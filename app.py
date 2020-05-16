077604e01a88dd2c3c805e68694a3bccd26a4534
BootCampUCD <56660232+BootCampUCD@users.noreply.github.com>
2020-05-15 20:22:03 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import dependencies
+import pandas as pd
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -13,7 +14,7 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 # Identify the database path
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///db/project_2.sql"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres:///db/project_2"
 # Make it so that it doesn't track modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -36,8 +37,56 @@ Base.prepare(engine,reflect = True)
 @app.route("/")
 def index():
     """Return the homepage."""
-    return render_template("https://BootCampUCD.github.io/index-P3-1.html
-")
+    return render_template("index-P3-1.html")
+
+# Define the route to "/unemployment"
+@app.route("/unemployment")
+def unemployment():
+    # Run a query to find unemployment data by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    unemployment = pd.read_sql("SELECT * FROM unemployment", engine)
+    return jsonify(unemployment.tolist())
+
+# Define the route to "/unemployment_state"
+@app.route("/unemployment_state")
+def unemployment():
+    # Run a query to find unemployment data by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    unemployment_state = pd.read_sql("SELECT * FROM unemployment_state", engine)
+    return jsonify(unemployment_state.tolist())
+
+# Define the route to "/mortgage_interest"
+@app.route("/mortgage_interest")
+def mortgage_interest():
+    # Run a query to find mortgage interest data by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    mortgage_interest = pd.read_sql("SELECT * FROM mortgage_interest", engine)
+    return jsonify(mortgage_interest.tolist())
+
+# Define the route to "/GDP_states"
+@app.route("/GDP_states")
+def GDP_states():
+    # Run a query to find GDP data by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    GDP_states = pd.read_sql("SELECT * FROM GDP_states", engine)
+    return jsonify(GDP_states.tolist())
+    
+# Define the route to "lat_lon"
+@app.route("/lat_lon")
+def lat_lon():
+    # Run a query to find latitude and longitude data by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    lat_lon = pd.read_sql("SELECT * FROM lat_lon", engine)
+    return jsonify(lat_lon.tolist())
+
+# Define the route to "state_pop"
+@app.route("/state_pop")
+def state_pop():
+    # Run a query to find state population data in millions by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    state_pop_in_m = pd.read_sql("SELECT * FROM state_pop", engine)
+    return jsonify(state_pop.tolist())
+
+# Define the route to "/state_pop_30m"
+@app.route("/state_pop_30m")
+def state_pop_30m():
+    # Run a query to find state population data which are 30 million by year and state (ANY ANALYSIS CAN BE CODED HERE before RETURN)
+    state_pop_30m = pd.read_sql("SELECT * FROM state_pop_30m", engine)
+    return jsonify(state_pop_30m.tolist())
 
     
 # Run the application
