9be4c51e0ccd2421e63ba3bfb70710948f057e13
BootCampUCD <56660232+BootCampUCD@users.noreply.github.com>
2020-05-17 09:04:12 -0700

    Add files via upload

--- app-P3.js
+++ app-P3.js
@@ -3,7 +3,7 @@
 
 // var url = "http://127.0.0.1:5501/Python/app.py";
 
-data1 = "https://BootCampUCD.github.io/P3-State-Lat-Long.geojson";
+data1 = "http://127.0.0.1:5501/Data/P3-State-Lat-Long.geojson";
 // data2 = "http://127.0.0.1:5501/Data/P3-unemployment.json";
 
 function commas(x) {
@@ -17,6 +17,12 @@ function decimal1(y) {
 function decimal2(y) {
   return y.toFixed(4);
 }
+const deathNumY = "";
+function adjustX(x) {
+  for (i = 0; i < datax.features.length; i++) {
+    deathNumY += datax.features[i].properties.Deaths;
+  }
+}
 
 // * * * This isolates the array of dictionaries into a list of States for the drop down * * *
 d3.json(data1).then(function (datax) {
@@ -26,8 +32,10 @@ d3.json(data1).then(function (datax) {
   const unEmpPop = [datax.features.length];
   const deathRate = [datax.features.length];
   const deathNum = [datax.features.length];
-  const deathNumX = [];
+  const deathNum2 = [datax.features.length];
+  const deathNumX = [datax.features.length];
   const statePop = [datax.features.length];
+  const statePopX = [];
   const percentOfTotal = [datax.features.length];
 
   for (i = 0; i < datax.features.length; i++) {
@@ -36,6 +44,7 @@ d3.json(data1).then(function (datax) {
     unEmpPop[i] = commas(datax.features[i].properties.UR_Pop);
     deathRate[i] = decimal1(datax.features[i].properties.Percent_D * 100);
     deathNum[i] = commas(datax.features[i].properties.Deaths);
+    deathNum2[i] = commas(datax.features[i].properties.Deaths * 500);
     statePop[i] = commas(datax.features[i].properties.Population_D);
     percentOfTotal[i] = decimal2(
       datax.features[i].properties.Deaths /
@@ -77,7 +86,7 @@ d3.json(data1).then(function (datax) {
         var demoArea = d3
           .select("#demoInfodata")
           .append("h5")
-          .text("Total Population: " + unEmpPop[i]);
+          .text("Total Population: " + statePop[i]);
         var demoArea = d3
           .select("#demoInfodata")
           .append("h5")
@@ -103,6 +112,7 @@ d3.json(data1).then(function (datax) {
       if (datax.features[i].properties.State == dataset) {
         var state = datax.features[i].properties.State;
         var deathNumX = commas(datax.features[i].properties.Deaths);
+        var statePopX = commas(datax.features[i].properties.Population_D);
         var sampleDataDisplay = d3
           .select("#bar")
           // .append("div") //If this is eliminated it will automatically clear the previous list when the next option is selected. So simple.
@@ -129,15 +139,29 @@ d3.json(data1).then(function (datax) {
     function ChartFormat() {
       var sampleDisplayArea = document.getElementById("bar");
       console.log("deathNum: " + deathNum);
+      console.log("statePop: " + statePop);
       var trace1 = {
-        type: "bar",
+        name: "COVID Deaths Enhanced by 500 times for visual realtivity",
+        type: "scatter",
         orientation: "v",
         x: states,
-        y: deathNum,
-        line: { color: "#17BECF" },
+        y: deathNum2,
+        line: { color: "red" },
         display: "inline-block",
       };
-      console.log(trace1);
+      // console.log(trace1);
+
+      var trace1a = {
+        name: "Total States Population",
+        type: "bar",
+        // mode: "markers",
+        x: states,
+        y: statePop,
+        // xaxis: "x2",
+        // yaxis: "y2",
+        line: { color: "blue" },
+        display: "inline-block",
+      };
 
       // var trace2 = {
       //   type: "scatter",
@@ -149,14 +173,13 @@ d3.json(data1).then(function (datax) {
       //   line: { color: "#17BECF" },
       //   display: "inline-block",
       // };
-      console.log("dropDownId: " + dropDownId[i]);
 
-      var data1 = [trace1];
+      var data = [trace1, trace1a];
       // var data2 = [trace2];
       // console.log(data);
       var layout1 = {
         height: 300,
-        width: 400,
+        width: 700,
         showlegend: true,
         legend: {
           text: "titleX",
@@ -165,48 +188,47 @@ d3.json(data1).then(function (datax) {
         title: "Number of COVID-19 Deaths",
         xaxis: {
           title: {
-            text: "x Axis",
+            text: "States",
           },
         },
         yaxis: {
           title: {
-            text: "y axis",
+            text: "Population Count",
           },
         },
       };
       console.log("Layout1:", layout1);
 
-      // var layout2 = {
-      //   height: 300,
-      //   width: 400,
-      //   showlegend: true,
-      //   legend: {
-      //     side: "left",
-      //   },
-      //   title: " Test Subject's Samples Data",
-      //   xaxis2: {
-      //     title: {
-      //       text: "x Axis",
-      //     },
-      //   },
-      //   yaxis2: {
-      //     title: {
-      //       text: "y axis",
-      //     },
-      //   },
-      // };
-      // console.log("Layout2:", layout2);
+      var layout2 = {
+        height: 300,
+        width: 400,
+        showlegend: true,
+        legend: {
+          side: "left",
+        },
+        title: " XYZ Subject's Samples Data",
+        xaxis2: {
+          title: {
+            text: "x Axis",
+          },
+        },
+        yaxis2: {
+          title: {
+            text: "y axis",
+          },
+        },
+      };
 
-      var data = [data1];
+      var layout = [layout1, layout2];
       Plotly.newPlot(
         "bar",
-        data1,
+        data,
         layout1,
         { displayModeBar: false },
         { scrollZoom: true }
       );
       // Plotly.newPlot(
-      //   "bubble",
+      //   "scatter",
       //   data2,
       //   layout2,
       //   { displayModeBar: false },
