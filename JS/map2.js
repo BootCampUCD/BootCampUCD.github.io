0645f2880dca6e374c0126bfe4f2120f209018bd
bootcampUCD <shoein2019@gmail.com>
2020-06-13 16:38:55 -0700

    Update map2.js

--- JS/map2.js
+++ JS/map2.js
@@ -1,4 +1,6 @@
 
+dataFile = "/Data/us-states.js";
+
 var mapboxAccessToken = {API_KEY};
 var map = L.map('map').setView([37.8, -96], 4);
 
@@ -10,6 +12,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     access_token: API_KEY,
 }).addTo(map);
 
-L.geoJson(Data/us-states.js).addTo(map);
+L.geoJson(dataFile).addTo(map);
 
 
