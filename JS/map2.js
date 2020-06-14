fa470656af99a07900b9096bef622122fa0ebdaa
bootcampUCD <shoein2019@gmail.com>
2020-06-13 18:00:04 -0700

    Update map2.js

--- JS/map2.js
+++ JS/map2.js
@@ -1,7 +1,6 @@
 var dataFile = "../Data/P3-State-Lat-Long.geojson";
-
 var mapboxAccessToken = { API_KEY };
-var map = L.map("map").setView([37.8, -96], 4);
+var myMap = L.map("map").setView([37.8, -96], 4);
 
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" +
@@ -10,8 +9,8 @@ L.tileLayer(
     id: "mapbox/light-v9",
     // attribution: ,
     tileSize: 512,
-    zoomOffset: -1,
+    maxZoom: 18,
   }
-).addTo(map);
+).addTo(myMap);
 
-L.geoJson(dataFile).addTo(map);
+L.geoJson(dataFile).addTo(myMap);
