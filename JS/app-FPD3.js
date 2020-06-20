0bd7b0810aea926791c493a32f92a42b01a17b55
bootcampUCD <shoein2019@gmail.com>
2020-06-20 10:22:35 -0700

    Update app-FPD3.js

--- JS/app-FPD3.js
+++ JS/app-FPD3.js
@@ -189,7 +189,7 @@ d3.csv("https://BootCampUCD.github.io/Data/data.csv")
       .attr("x", 0)
       .attr("y", 20)
       .classed("axis-text", true)
-      .text("2-Poverty %")
+      .text("3-Poverty %")
       .style("font", "35px times")
       .style("fill", "white")
       .style("stroke", "white");
