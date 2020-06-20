c3bd36087312ac2c36a3cd1370aba8771767ce2e
bootcampUCD <shoein2019@gmail.com>
2020-06-19 19:26:41 -0700

    Update app-FPD3.js

--- JS/app-FPD3.js
+++ JS/app-FPD3.js
@@ -170,7 +170,7 @@ d3.csv("https://BootCampUCD.github.io/Data/data.csv")
       .attr("x", (d, i) => xLinearScale(poverty[i]))
       .attr("y", (d, i) => yLinearScale(healthcare[i]))
       .text((d, i) => abbr[i])
-      .style("color", "white")
+      .style("stroke", "white")
       .style("font-weight", "bold");
 
     // * * * x-axis labels
