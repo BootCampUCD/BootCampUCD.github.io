b3f3be0d2d4235a0cd1560521b47c7418baadb82
bootcampUCD <shoein2019@gmail.com>
2020-06-20 20:01:12 -0700

    Update app-FPD3.js

--- JS/app-FPD3.js
+++ JS/app-FPD3.js
@@ -115,7 +115,7 @@ d3.csv("https://BootCampUCD.github.io/Data/data.csv")
     var chosenXAxis = "poverty"; //allows changing of data source comparison-not active
     var xLinearScale = d3
       .scaleLinear()
-      .domain([0, d3.max(data1, (d, i) => poverty[i])])
+      .domain([0, d3.max(data1, (d, i) => poverty[i]) * 3])
       // .domain([0, d3.max(data1, (d) => d.poverty)]) //`d.${chosenXAxis}`
       .range([0, width]); //manages the display width on the screen
     console.log(data1.length);
@@ -124,7 +124,7 @@ d3.csv("https://BootCampUCD.github.io/Data/data.csv")
     var yLinearScale = d3
       .scaleLinear()
       .domain([0, d3.max(data1, (d) => d.healthcare) * 3])
-      .range([height + 5, 0]); //manages the display height on the screen
+      .range([height, 0]); //manages the display height on the screen
 
     // Create initial axis functions
     var bottomAxis = d3.axisBottom(xLinearScale);
