7d2697e97ee130da766cc15847632ccc895aeeaf
bootcampUCD <shoein2019@gmail.com>
2020-06-20 20:04:32 -0700

    Update app-FPD3.js

--- JS/app-FPD3.js
+++ JS/app-FPD3.js
@@ -205,7 +205,7 @@ d3.csv("https://BootCampUCD.github.io/Data/data.csv")
       .attr("x", 0 - height) //(height/2) or (height*.75)
       .attr("dy", "3em")
       .classed("axis-text", true)
-      .text("5-Healthcare %")
+      .text("Healthcare %")
       .style("font", "35px times")
       .style("fill", "white")
       .style("stroke", "white");
