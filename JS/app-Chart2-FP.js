9d7c2a13b0b5f766de88baae7155214c49691e7a
bootcampUCD <shoein2019@gmail.com>
2020-06-24 17:40:24 -0700

    Update app-Chart2-FP.js

--- JS/app-Chart2-FP.js
+++ JS/app-Chart2-FP.js
@@ -1,145 +1,102 @@
 // Pulling the data from a file via local web hosting.
 // Performing data manipulation within a function.
 
-// d3.select("#demoInfodata").text(
-//   "Each Year's Population Data Will Display Here After Choosing a Year to Compare."
-// );
-x = 0;
-if ((x = 0)) {
-  x = x + 1;
-  d3.select("#demoInfodata").text(
-    "Each Year's Population Data Will Display Here After Choosing a Year to Compare."
-  );
-  location.reload();
-} else {
-  y = 1;
-}
-
-// var url = "http://127.0.0.1:5501/Python/app.py";
-
-data1 = "https://BootCampUCD.github.io/Data/P3-State-Lat-Long.geojson";
-// data2 = "http://127.0.0.1:5501/Data/P3-unemployment.json";
-// data3 = "http://127.0.0.1:5432/Data/P3-State-Lat-Long.geojson".then();
-// data4 = "https://BootCampUCD.github.io/Data/data.csv";
-
-console.log(data1);
-
-function commas(x) {
-  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function decimal1(y) {
-  return y.toFixed(2);
-}
-
-function decimal2(y) {
-  return y.toFixed(4);
-}
-const deathNumY = "";
-function adjustX(x) {
-  for (i = 0; i < datax.features.length; i++) {
-    deathNumY += datax.features[i].properties.Deaths;
-  }
-}
-
-// * * * This isolates the array of dictionaries into a list of States for the drop down * * *
-d3.json(data1).then(function (datax) {
+d3.json("https://bootcampucd.github.io/samples.json").then(function (data) {
+  var nameNum = data.names;
+  var metaData = data.metadata;
+  var samplesData = data.samples;
+  console.log("samplesData begin:", samplesData);
   var displayStatus = 0;
-  const states = [datax.features.length];
-  const unEmpRate = [datax.features.length];
-  const unEmpPop = [datax.features.length];
-  const deathRate = [datax.features.length];
-  const deathNum = [datax.features.length];
-  const deathNum2 = [datax.features.length];
-  const deathNumX = [datax.features.length];
-  const statePop = [datax.features.length];
-  const statePopX = [];
-  const percentOfTotal = [datax.features.length];
-
-  for (i = 0; i < datax.features.length; i++) {
-    states[i] = datax.features[i].properties.State;
-    unEmpRate[i] = datax.features[i].properties.UR;
-    unEmpPop[i] = commas(datax.features[i].properties.UR_Pop);
-    deathRate[i] = decimal1(datax.features[i].properties.Percent_D * 100);
-    deathNum[i] = commas(datax.features[i].properties.Deaths);
-    deathNum2[i] = commas(datax.features[i].properties.Deaths * 500);
-    statePop[i] = commas(datax.features[i].properties.Population_D);
-    percentOfTotal[i] = decimal2(
-      datax.features[i].properties.Deaths /
-        datax.features[i].properties.Population_D
-    );
-  }
-  // * * * END * * *This isolates the data for display * * *
 
   function optionChanged() {
     var dataset = d3.selectAll("#selDataset").property("value"); //retrieves data selected from dropdown
+    console.log("testing: ", dataset);
     //Types a message if "undefined" is selected from drop down list.
     if (dataset == "undefined") {
       var demoArea = d3
         .select("#demoInfodata")
         .text(
-          "Each Year's Population Data Will Display Here After Choosing a Year to Compare."
+          "Each Test Subject's Demographic Data Will Display Here After Choosing a Subject ID Number Above."
         );
       var sampleDisplayArea = document.getElementById("bar"); // identifies display area tags
       var sampleDataDisplay = d3.select("#bar").text(""); //clears previous data displays.
     }
     // console.log("Selected from Dropdown list", dataset);
     var displayArea = document.getElementById("demoInfodata"); // identifies display area tags
-    // console.log("displayArea--", displayArea);
-    for (i = 0; i < datax.features.length; i++) {
-      if (datax.features[i].properties.State == dataset) {
-        // var state = datax[i].State;
-        // var unemp_rate = data.unemp_rate[i];
-        // var bbtype = metaData[i].bbtype;
-        // var ethnicity = metaData[i].ethnicity;
-        // var gender = metaData[i].gender;
-        // var id = metaData[i].id;
-        // var location = metaData[i].location;
-        // var wfreq = metaData[i].wfreq;
+    console.log("displayArea--", displayArea);
+    console.log("dataset value:", dataset);
+    console.log(displayArea);
+    console.log("metaData length:", metaData.length);
+
+    for (i = 0; i < metaData.length; i++) {
+      // console.log(i + ",0", metaData[i]);
+      if (metaData[i].id == dataset) {
+        var age = metaData[i].age;
+        var bbtype = metaData[i].bbtype;
+        var ethnicity = metaData[i].ethnicity;
+        var gender = metaData[i].gender;
+        var id = metaData[i].id;
+        var location = metaData[i].location;
+        var wfreq = metaData[i].wfreq;
         console.log("demoArea b4", demoArea);
+        // var demoArea = d3.select("panel-body").select("#demoInfodata").html("");
         var demoArea = d3
           .select("#demoInfodata")
           // .append("h5") //If this is eliminated it will automatically clear the previous list when the next option is selected.  So simple.
-          .text("State: " + states[i]);
+          .text("id:" + id);
         var demoArea = d3
           .select("#demoInfodata")
           .append("h5")
-          .text("Total Population: " + statePop[i]);
+          .text("ethnicity: " + ethnicity);
         var demoArea = d3
           .select("#demoInfodata")
           .append("h5")
-          .text("COVID-19 Deaths: " + deathNum[i] + " (" + deathRate[i] + "%)");
+          .text("gender: " + gender);
         var demoArea = d3
           .select("#demoInfodata")
           .append("h5")
-          .text(
-            "State vs Nation of COVID-19 Deaths: " +
-              " (" +
-              percentOfTotal[i] +
-              "%)"
-          );
+          .text("age: " + age);
+        var demoArea = d3
+          .select("#demoInfodata")
+          .append("h5")
+          .text("location: " + location);
+        var demoArea = d3
+          .select("#demoInfodata")
+          .append("h5")
+          .text("bbtype: " + bbtype);
+        var demoArea = d3
+          .select("#demoInfodata")
+          .append("h5")
+          .text("wfreq: " + wfreq);
       }
     }
-    chartSamplesData(datax);
+    chartSamplesData(samplesData);
   }
   function chartSamplesData(item) {
     var dataset = d3.selectAll("#selDataset").property("value"); //retrieves data selected from dropdown
     console.log("testing2-", dataset);
     var sampleDisplayArea = document.getElementById("bar"); // identifies display area tags
-    for (i = 0; i < datax.features.length; i++) {
-      if (datax.features[i].properties.State == dataset) {
-        var state = datax.features[i].properties.State;
-        var deathNumX = commas(datax.features[i].properties.Deaths);
-        var statePopX = commas(datax.features[i].properties.Population_D);
-        var sampleDataDisplay = d3
-          .select("#bar")
-          // .append("div") //If this is eliminated it will automatically clear the previous list when the next option is selected. So simple.
-          .text("State: " + state + " COVID-19 Deaths: " + deathNumX);
+    for (i = 0; i < samplesData.length; i++) {
+      if (samplesData[i].id == dataset) {
+        console.log("samplesData: #" + i, samplesData[i].id);
+        console.log("samplesData array: #" + i, samplesData[i]);
+        var id = samplesData[i].id;
+        var otu_ids = samplesData[i].otu_ids;
+        var otu_labels = samplesData[i].otu_labels;
+        var sample_values = samplesData[i].sample_values;
+        console.log("id: ", id);
+        console.log("otu_id b4", otu_ids);
+        console.log("otu_labels b4", otu_labels);
+        console.log("sample_values b4", sample_values);
+        // var sampleDataDisplay = d3
+        //   .select("#bar")
+        //   // .append("div") //If this is eliminated it will automatically clear the previous list when the next option is selected. So simple.
+        //   .text("id: " + id);
         // console.log("ID length:", id.length);
         // var sampleDataDisplay = d3
         //   .select("#bar")
         //   .append("div")
-        //   .text("COVID-19 Deaths: " + deathNumX);
+        //   .text("otu_ids: " + otu_ids);
         // console.log("otu_ids length:", otu_ids.length);
         // var sampleDataDisplay = d3
         //   .select("#bar")
@@ -156,102 +113,55 @@ d3.json(data1).then(function (datax) {
 
     function ChartFormat() {
       var sampleDisplayArea = document.getElementById("bar");
-      console.log("deathNum: " + deathNum);
-      console.log("statePop: " + statePop);
+
       var trace1 = {
-        name: "COVID Deaths Enhanced 500 times for visual realtivity",
-        type: "scatter",
-        orientation: "v",
-        x: states,
-        y: deathNum2,
-        line: { color: "red" },
-        display: "inline-block",
-      };
-      // console.log(trace1);
-
-      var trace1a = {
-        name: "Total States Population",
         type: "bar",
-        // mode: "markers",
-        x: states,
-        y: statePop,
-        // xaxis: "x2",
-        // yaxis: "y2",
-        line: { color: "blue" },
-        display: "inline-block",
+        orientation: "h",
+        x: sample_values,
+        y: otu_ids,
+        line: { color: "#17BECF" },
       };
+      console.log(trace1);
 
-      // var trace2 = {
-      //   type: "scatter",
-      //   mode: "markers",
-      //   x: sample_values,
-      //   y: otu_ids,
-      //   xaxis: "x2",
-      //   yaxis: "y2",
-      //   line: { color: "#17BECF" },
-      //   display: "inline-block",
-      // };
+      var trace2 = {
+        type: "scatter",
+        mode: "markers",
+        x: sample_values,
+        y: otu_ids,
+        line: { color: "#17BECF" },
+      };
+      console.log(trace2);
 
-      var data = [trace1, trace1a];
-      // var data2 = [trace2];
-      // console.log(data);
+      var data1 = [trace1];
+      var data2 = [trace2];
+      console.log(data);
       var layout1 = {
-        height: 500,
-        width: 900,
-        showlegend: true,
+        height: 600,
+        witdth: 600,
         legend: {
-          text: "titleX",
+          showlegend: true,
           side: "left",
         },
-        title: "Number of COVID-19 Deaths",
-        xaxis: {
-          title: {
-            text: "States",
-          },
-        },
-        yaxis: {
-          title: {
-            text: "Population Count",
-          },
-        },
+        title: " Test Subject's Samples Data",
+        xaxis: {},
+        yaxis: {},
       };
       console.log("Layout1:", layout1);
 
-      // var layout2 = {
-      //   height: 300,
-      //   width: 700,
-      //   showlegend: true,
-      //   legend: {
-      //     side: "left",
-      //   },
-      //   title: " XYZ Subject's Samples Data",
-      //   xaxis2: {
-      //     title: {
-      //       text: "x Axis",
-      //     },
-      //   },
-      //   yaxis2: {
-      //     title: {
-      //       text: "y axis",
-      //     },
-      //   },
-      // };
-
-      var layout = [layout1, layout2];
-      Plotly.newPlot(
-        "bar",
-        data,
-        layout1,
-        { displayModeBar: false },
-        { scrollZoom: true }
-      );
-      // Plotly.newPlot(
-      //   "scatter",
-      //   data2,
-      //   layout2,
-      //   { displayModeBar: false },
-      //   { scrollZoom: true }
-      // );
+      var layout2 = {
+        height: 600,
+        witdth: 600,
+        legend: {
+          showlegend: true,
+          side: "left",
+        },
+        title: " Test Subject's Samples Data",
+        xaxis: {},
+        yaxis: {},
+      };
+      console.log("Layout2:", layout2);
+      Plotly.newPlot("bar", data1, layout1, { scrollZoom: true });
+      Plotly.newPlot("bubble", data2, layout2);
     }
 
     // function updateChartData() {
@@ -266,13 +176,12 @@ d3.json(data1).then(function (datax) {
     option.text = item; //define the list of data for the dropdown as a string
     select.add(option); //displays the entire list of data for the dropdown in a vertical list
   }
+
   d3.selectAll("#selDataset").on("change", optionChanged);
   //set displayed value to "undefined" forcing a User choice before printing any data.
   addToDropdown(dropDownId);
-
-  // * * * Assign List of States to the Drop down prompt * * *
-  var dropDownId = states;
+  //Assign List of Test Subject ID.
+  var dropDownId = nameNum;
   //cycles through the array and adds each key to the drop down list.
   dropDownId.forEach(addToDropdown);
-  // * * * END * * * Assign List of States to the Drop down prompt * * *
 });
